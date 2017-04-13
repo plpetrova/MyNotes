ae23f167afbddf6a60c69dcec312a2a037a0e47a
plpetrova <plamena.l.petrova@gmail.com>
2017-04-13 23:08:00 +0300

    Add files via upload

---
+++ MyNotes.DataBase/dbo/Tables/Notes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [NoteID]         INT  IDENTITY (1, 1) NOT NULL,
+    [UserID]         INT  NOT NULL,
+    [NoteContent]    TEXT NOT NULL,
+    [DateOfCreation] DATE NOT NULL,
+    [EndTime]        DATE NULL,
+    CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED ([NoteID] ASC),
+    CONSTRAINT [FK_Notes_User] FOREIGN KEY ([UserID]) REFERENCES [dbo].[User] ([UserID])
+);
+
---
+++ MyNotes.DataBase/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserID]   INT           IDENTITY (1, 1) NOT NULL,
+    [UserName] VARCHAR (250) NOT NULL,
+    [Password] VARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
